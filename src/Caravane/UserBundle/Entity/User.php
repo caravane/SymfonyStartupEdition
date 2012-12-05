7b2d0d5257bb8b3fe4821aad390b60d22ba8e59a
EricGrivilers <eric@caravanemedia.com>
2012-12-05 23:18:21 +0100

    Update src/Caravane/UserBundle/Entity/User.php
    a

--- src/Caravane/UserBundle/Entity/User.php
+++ src/Caravane/UserBundle/Entity/User.php
@@ -5,8 +5,7 @@ use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
- * @ORM\Table(name="fos_user")
+ * @MongoDB\Document
  */
 class User extends BaseUser
 {
@@ -29,11 +28,47 @@ class User extends BaseUser
      */
     protected $lastname;
 
+
     /**
      * @var string
      * @ORM\Column(type="string", nullable=true)
      */
     protected $facebookID;
+
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $twitterID;
+
+     /**
+     * @var string
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $googleID;
+
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $twitter_username;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $facebook_username;
+
+      /**
+     * @var string
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $avatar;
+
+
+
 
     public function __construct()
     {
@@ -43,17 +78,92 @@ class User extends BaseUser
 
     public function serialize()
     {
-        return serialize(array($this->facebookID, parent::serialize()));
+        return serialize(array($this->facebookID, $this->twitterID,  parent::serialize()));
     }
 
     public function unserialize($data)
     {
-        list($this->facebookID, $parentData) = unserialize($data);
+        list($this->facebookID, $this->twitterID,  $parentData) = unserialize($data);
         parent::unserialize($parentData);
     }
 
     /**
-     * @return string
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set facebookID
+     *
+     * @param string $facebookID
+     * @return \User
+     */
+    public function setFacebookID($facebookID)
+    {
+        $this->facebookID = $facebookID;
+        $this->setUsername("fb_".$facebookID);
+        return $this;
+    }
+
+    /**
+     * Get facebookID
+     *
+     * @return string $facebookID
+     */
+    public function getFacebookID()
+    {
+        return $this->facebookID;
+    }
+
+
+    /**
+     * @param Array
+     */
+    public function setFBData($data)
+    {
+        if (isset($data['id'])) {
+            $this->setFacebookID($data['id']);
+            $this->addRole('ROLE_USER');
+        }
+        if (isset($data['username'])) {
+            $this->setFacebookUsername($data['username']);
+
+            $this->setAvatar($avatar="https://graph.facebook.com/".$data['username']."/picture");
+        }
+        if (isset($data['first_name'])) {
+            $this->setFirstname($data['first_name']);
+        }
+        if (isset($data['last_name'])) {
+            $this->setLastname($data['last_name']);
+        }
+        if (isset($data['email'])) {
+            $this->setEmail($data['email']);
+        }
+    }
+
+
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     * @return \User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string $firstname
      */
     public function getFirstname()
     {
@@ -61,15 +171,21 @@ class User extends BaseUser
     }
 
     /**
-     * @param string $firstname
+     * Set lastname
+     *
+     * @param string $lastname
+     * @return \User
      */
-    public function setFirstname($firstname=null)
+    public function setLastname($lastname)
     {
-        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        return $this;
     }
 
     /**
-     * @return string
+     * Get lastname
+     *
+     * @return string $lastname
      */
     public function getLastname()
     {
@@ -77,73 +193,125 @@ class User extends BaseUser
     }
 
     /**
-     * @param string $lastname
-     */
-    public function setLastname($lastname=null)
-    {
-        $this->lastname = $lastname;
-    }
-
-    /**
-     * Get the full name of the user (first + last name)
-     * @return string
-     */
-    public function getFullName()
-    {
-        return $this->getFirstName() . ' ' . $this->getLastname();
-    }
-
-    /**
-     * @param string $facebookID
-     * @return void
-     */
-    public function setFacebookID($facebookID=null)
-    {
-        $this->facebookID = $facebookID;
-        if ($this->username=="")    //si on n'a pas de username
+         * Set twitterID
+         *
+         * @param string $twitterID
+         */
+        public function setTwitterID($twitterID)
         {
-            //on met le facebook id a la place
-            $this->setUsername($facebookID);
+            $this->twitterID = $twitterID;
+            $this->setUsername("tw_".$twitterID);
+            $this->salt = '';
         }
-        $this->salt = '';
-    }
+
+        /**
+         * Get twitterID
+         *
+         * @return string
+         */
+        public function getTwitterID()
+        {
+            return $this->twitterID;
+        }
+
+        /**
+         * Set twitter_username
+         *
+         * @param string $twitterUsername
+         */
+        public function setTwitterUsername($twitterUsername)
+        {
+            $this->twitter_username = $twitterUsername;
+        }
+
+        /**
+         * Get twitter_username
+         *
+         * @return string
+         */
+        public function getTwitterUsername()
+        {
+            return $this->twitter_username;
+        }
+
+
+
+        /**
+         * Set facebook_username
+         *
+         * @param string $facebookUsername
+         */
+        public function setFacebookUsername($facebookUsername)
+        {
+            $this->facebook_username = $facebookUsername;
+        }
+
+        /**
+         * Get facebook_username
+         *
+         * @return string
+         */
+        public function getFacebookUsername()
+        {
+            return $this->facebook_username;
+        }
+
+
 
     /**
-     * @return string
-     */
-    public function getFacebookID()
-    {
-        return $this->facebookID;
-    }
-
-    /**
-     * @param Array
-     */
-    public function setFBData($fbdata)
-    {
-        if (isset($fbdata['id'])) {
-            $this->setFacebookID($fbdata['id']);
-            $this->addRole('ROLE_FACEBOOK');
-        }
-        if (isset($fbdata['first_name'])) {
-            $this->setFirstname($fbdata['first_name']);
-        }
-        if (isset($fbdata['last_name'])) {
-            $this->setLastname($fbdata['last_name']);
-        }
-        if (isset($fbdata['email'])) {
-            $this->setEmail($fbdata['email']);
-        }
-    }
-
-
-    /**
-     * Get id
+     * Set avatar
      *
-     * @return integer
+     * @param string $facebookAvatar
+     * @return \User
      */
-    public function getId()
+    public function setAvatar($avatar)
     {
-        return $this->id;
+        $this->avatar = $avatar;
+        return $this;
+    }
+
+    /**
+     * Get avatar
+     *
+     * @return string $facebookAvatar
+     */
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+
+    /**
+     * Set googleID
+     *
+     * @param string $googleID
+     * @return \User
+     */
+    public function setGoogleID($googleID)
+    {
+        $this->googleID = $googleID;
+        return $this;
+    }
+
+    /**
+     * Get googleID
+     *
+     * @return string $googleID
+     */
+    public function getGoogleID()
+    {
+        return $this->googleID;
+    }
+
+    public function getName() {
+        if(isset($this->firstname) || isset($this->lastname)) {
+            $name=$this->firstname." ".$this->lastname;
+        }
+        else if(isset($this->twitter_username)) {
+            $name=$this->twitter_username;
+        }
+        else {
+            $name=$this->username;
+        }
+        return $name;
     }
 }
